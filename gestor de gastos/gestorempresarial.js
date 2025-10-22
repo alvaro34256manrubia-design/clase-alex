928b7741ab3b8a769ed9aa1d60733ca07476b1df
alcalman <alvaro.34256manrubia@gmail.com>
2025-10-22 16:45:56 +0200

    anadiendo funciones mejoradas

--- gestor de gastos/gestorempresarial.js
+++ gestor de gastos/gestorempresarial.js
@@ -12,22 +12,27 @@ function actualizarPresupuesto(nuevoValor) {
 }
 
 function mostrarPresupuesto(){
-    console.log("Presupuessto",presupuesto,"€")
+   document.getElementById("mostrarPresupuesto").textContent =
+    "Tu presupuesto actual es de " + presupuesto + " €";
 }
 
-function Gasto(descripcion, valor) {
-  if (valor < 0) {
-    console.error(presupuesto = 0);
-    
+function CrearGasto(descripcion, valor) {
+  let gasto = {
+    descripcion: descripcion,
+      valor: (isNaN(valor) || valor < 0) ? 0 : parseFloat(valor),
+      mostrarGasto: function() {
+        return "Gasto: " + this.descripcion + " - " + this.valor + " €";
+      }
+    };
+    return gasto;
   }
-
   this.descripcion = descripcion;
   this.valor = valor;
 
   this.mostrar = function() {
     console.log(`Gasto: "${this.descripcion}" - Valor: ${this.valor} €`);
   };
-}
+
 
 descripcion = String;
 valor = Number;
